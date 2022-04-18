07f4875faa843637221c377e44764d585c8f4565
findxc <liuxiaocui1993@gmail.com>
2022-04-18 15:45:34 +0800

    用户列表加上表格展示

--- src/app/pages/user/list/list.component.ts
+++ src/app/pages/user/list/list.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { NzTableQueryParams } from 'ng-zorro-antd/table';
+
+interface User {
+  id: string;
+  name: string;
+  gender: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  list: User[] = [];
+  loading = true;
+  total = 0;
+  pageSize = 10;
+  pageIndex = 1;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onQueryParamsChange(params: NzTableQueryParams): void {
+    const { pageSize, pageIndex } = params;
+    this.fetchList(pageIndex, pageSize);
   }
 
+  fetchList(pageIndex: number, pageSize: number) {
+    this.loading = true;
+    this.pageIndex = pageIndex;
+    this.pageSize = pageSize;
+
+    setTimeout(() => {
+      this.list = new Array(10).fill('').map((_, index) => {
+        const id = String(index + 1 + pageSize * (pageIndex - 1));
+        return {
+          id,
+          name: `name${id}`,
+          gender: `gender${id}`,
+          email: `email${id}`,
+        };
+      });
+      this.total = 25;
+      this.loading = false;
+    }, 500);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 import { UserRoutingModule } from './user-routing.module';
 import { ListComponent } from './list/list.component';
@@ -7,13 +8,7 @@ import { LogComponent } from './log/log.component';
 
 
 @NgModule({
-  declarations: [
-    ListComponent,
-    LogComponent
-  ],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [ListComponent, LogComponent],
+  imports: [CommonModule, UserRoutingModule, NzTableModule],
 })
-export class UserModule { }
+export class UserModule {}
