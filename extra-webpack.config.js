29fbe99e5470aead2c8784c8afcdc6694faf1244
findxc <liuxiaocui1993@gmail.com>
2022-06-06 16:58:56 +0800

    support custom webpack config

---
+++ extra-webpack.config.js
@@ -0,0 +1,10 @@
+const { DefinePlugin } = require("webpack");
+
+module.exports = (config, options, targetOptions) => {
+  config.plugins.push(
+    new DefinePlugin({
+      APP_VERSION: JSON.stringify("v1.0.0"),
+    })
+  );
+  return config;
+};
