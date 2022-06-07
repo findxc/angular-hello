bf1e8cccf22d08bd7cb20b23aa8e4603d51bb397
findxc <liuxiaocui1993@gmail.com>
2022-06-07 19:39:05 +0800

    add login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './core/components/layout/layout.component';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   {
-    path: 'welcome',
-    loadChildren: () =>
-      import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
-  },
-  {
-    path: 'user',
-    loadChildren: () =>
-      import('./pages/user/user.module').then((m) => m.UserModule),
-  },
-  {
-    path: 'stat',
-    loadChildren: () =>
-      import('./pages/stat/stat.module').then((m) => m.StatModule),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'welcome',
+        loadChildren: () =>
+          import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+      },
+      {
+        path: 'user',
+        loadChildren: () =>
+          import('./pages/user/user.module').then((m) => m.UserModule),
+      },
+      {
+        path: 'stat',
+        loadChildren: () =>
+          import('./pages/stat/stat.module').then((m) => m.StatModule),
+      },
+    ],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IconsProviderModule } from './icons-provider.module';
+
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+import { IconsProviderModule } from './icons-provider.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './core/components/layout/layout.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LayoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,9 +28,9 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     IconsProviderModule,
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
