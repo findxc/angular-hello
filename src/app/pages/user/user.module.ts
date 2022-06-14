2dbdf38a61e8dd9ca1e2d663515bbf4bdd7ea383
findxc <liuxiaocui1993@gmail.com>
2022-06-14 14:58:29 +0800

    add shared module

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { ReactiveFormsModule } from '@angular/forms'
-import { NzTableModule } from 'ng-zorro-antd/table'
-import { NzModalModule } from 'ng-zorro-antd/modal'
-import { NzButtonModule } from 'ng-zorro-antd/button'
-import { NzFormModule } from 'ng-zorro-antd/form'
-import { NzInputModule } from 'ng-zorro-antd/input'
-
+import { SharedModule } from '../../shared/shared.module'
 import { UserRoutingModule } from './user-routing.module'
 import { ListComponent } from './list/list.component'
 import { LogComponent } from './log/log.component'
@@ -14,15 +7,6 @@ import { UserEditModalComponent } from './user-edit-modal/user-edit-modal.compon
 
 @NgModule({
   declarations: [ListComponent, LogComponent, UserEditModalComponent],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    UserRoutingModule,
-    NzTableModule,
-    NzModalModule,
-    NzButtonModule,
-    NzFormModule,
-    NzInputModule,
-  ],
+  imports: [UserRoutingModule, SharedModule],
 })
 export class UserModule {}
