5958a879f51cdc6fe6e05c63846e48aac42a9434
findxc <liuxiaocui1993@gmail.com>
2022-07-14 00:51:32 +0800

    tmp 1

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -11,8 +11,8 @@ import { MyCardComponent } from '@shared/components/my-card/my-card.component'
   template: `
     <div>hello, {{ version }}</div>
     <app-my-card #card title="111">
-      <div appCardContent>11</div>
-      <div appCardContent>22</div>
+      <div *appCardContent>11</div>
+      <div *appCardContent>22</div>
     </app-my-card>
     <app-my-card #card title="222"></app-my-card>
   `,
